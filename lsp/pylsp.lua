a9e47e9066a08bf53642347d5680308a42026aaf
Kazeo Hada <48868375+kazeohada@users.noreply.github.com>
2025-11-07 01:24:32 +0100

    pylsp

---
+++ lsp/pylsp.lua
@@ -0,0 +1,53 @@
+---@brief
+---
+--- https://github.com/python-lsp/python-lsp-server
+---
+--- A Python 3.6+ implementation of the Language Server Protocol.
+---
+--- See the [project's README](https://github.com/python-lsp/python-lsp-server) for installation instructions.
+---
+--- Configuration options are documented [here](https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md).
+--- In order to configure an option, it must be translated to a nested Lua table and included in the `settings` argument to the `config('pylsp', {})` function.
+--- For example, in order to set the `pylsp.plugins.pycodestyle.ignore` option:
+--- ```lua
+--- vim.lsp.config('pylsp', {
+---   settings = {
+---     pylsp = {
+---       plugins = {
+---         pycodestyle = {
+---           ignore = {'W391'},
+---           maxLineLength = 100
+---         }
+---       }
+---     }
+---   }
+--- })
+--- ```
+---
+--- Note: This is a community fork of `pyls`.
+
+---@type vim.lsp.Config
+return {
+  cmd = { 'pylsp' },
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    '.git',
+  },
+  settings = {
+    pylsp = {
+      plugins = {
+        pyflakes = { enabled = true },       -- detects syntax errors & unused vars
+        pycodestyle = { enabled = true },    -- enforces PEP8 style
+        jedi_completion = { fuzzy = true },
+        jedi_definition = { enabled = true }, -- enables `gd`
+        jedi_references = { enabled = true },
+        jedi_hover = { enabled = true },
+      },
+    },
+  },
+}
