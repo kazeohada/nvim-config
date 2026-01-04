256830cb86950aae2a6b06dc7444ff908ddc009c
Kazeo Hada <kazeohada@gmail.com>
2026-01-04 22:26:37 +0100

    treesitter uses master branch

--- lua/kazeo/packer.lua
+++ lua/kazeo/packer.lua
@@ -13,7 +13,11 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'}}
   }
 
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {
+      'nvim-treesitter/nvim-treesitter', 
+      branch = 'master', 
+      run = ':TSUpdate'
+  }
   use ('nvim-treesitter/playground')
   use ('theprimeagen/harpoon')
   use ('mbbill/undotree')
