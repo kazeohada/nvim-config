479ebefaeb5f51fb51ee1238b91acc81df4b67ee
Kazeo Hada <kahada@splunk.com>
2026-01-05 09:19:19 +0100

    commented gitgutter for now

--- lua/kazeo/packer.lua
+++ lua/kazeo/packer.lua
@@ -23,7 +23,7 @@ return require('packer').startup(function(use)
   use ('mbbill/undotree')
   use ('tpope/vim-fugitive')
   use ('sindrets/diffview.nvim')
-  use ('airblade/vim-gitgutter')
+  -- use ('airblade/vim-gitgutter')
 
   use({
     "iamcco/markdown-preview.nvim",
