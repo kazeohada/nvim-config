a23bc58d0c852163ef1b483e12cf86374aa89569
Kazeo Hada <kazeohada@gmail.com>
2026-01-25 00:03:45 +0100

    luasnip plugin

---
+++ after/plugin/luasnip.lua
@@ -0,0 +1,11 @@
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local ls = require("luasnip")
+local types = require("luasnip.util.types")
+
+ls.config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+
+    enable_autosnippets = true
+}
--- lua/kazeo/packer.lua
+++ lua/kazeo/packer.lua
@@ -4,41 +4,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'}}
-  }
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'}}
+    }
 
-  use {
+    use {
       'nvim-treesitter/nvim-treesitter', 
       branch = 'master', 
       run = ':TSUpdate'
-  }
-  use ('nvim-treesitter/playground')
-  use ('theprimeagen/harpoon')
-  use ('mbbill/undotree')
-  use ('tpope/vim-fugitive')
-  use ('sindrets/diffview.nvim')
-  -- use ('airblade/vim-gitgutter')
-  
+    }
+    use ('nvim-treesitter/playground')
+    use ('theprimeagen/harpoon')
+    use ('mbbill/undotree')
+    use ('tpope/vim-fugitive')
+    use ('sindrets/diffview.nvim')
+    -- use ('airblade/vim-gitgutter')
 
-  use ('hrsh7th/nvim-cmp')
-  use ('hrsh7th/cmp-nvim-lsp')
-  use ('hrsh7th/cmp-vsnip')
-  use ('hrsh7th/vim-vsnip')
+    use ('hrsh7th/nvim-cmp')
+    use ('hrsh7th/cmp-nvim-lsp')
+    use ('hrsh7th/cmp-vsnip')
+    use ('hrsh7th/vim-vsnip')
 
-  use({
+    use({
+        "L3MON4D3/LuaSnip",
+        tag = "v2.*", 
+        run = "make install_jsregexp"
+    })
+    use ('rafamadriz/friendly-snippets')
+
+    use({
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
-  })
+    })
 
-  use ('hashivim/vim-terraform')
+    use ('hashivim/vim-terraform')
 
-  use ('rebelot/kanagawa.nvim')
+    use ('rebelot/kanagawa.nvim')
 
-  use { 'martineausimon/nvim-lilypond-suite' }
+    use { 'martineausimon/nvim-lilypond-suite' }
 end)
