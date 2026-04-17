8a18de6c6bb4c5936f6e636a5b151cd4dbd35fae
Kazeo Hada <kahada@splunk.com>
2026-04-17 15:28:01 +0200

    moving window in terminal

--- lua/kazeo/remap.lua
+++ lua/kazeo/remap.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
 vim.keymap.set({'n', 'v'}, 'Y', '"+y', { noremap = true })
-
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { desc = "Move to left window" })
+vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { desc = "Move to top window" })
+vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l", { desc = "Move to right window" })
