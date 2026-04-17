bba5a8fe1fe3bd32eafbee7e779ee0e97d309c06
Kazeo Hada <kahada@splunk.com>
2026-04-17 15:28:38 +0200

    commit and push fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,11 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", function()
+    local buf = vim.fn.bufnr("fugitive://")
+    if buf ~= -1 and vim.fn.bufwinnr(buf) ~= -1 then
+        vim.cmd("bdelete " .. buf)
+    else
+        vim.cmd.Git()
+    end
+end, { desc = "Toggle Fugitive" })
+
+vim.keymap.set("n", "<leader>gc", function() vim.cmd("Git commit") end, { desc = "Git commit" })
+vim.keymap.set("n", "<leader>gp", function() vim.cmd("Git push") end, { desc = "Git push" })
