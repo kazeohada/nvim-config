2cfd92323ffe48b794f2f839e46e73d22e3bba1d
Kazeo Hada <kahada@splunk.com>
2026-06-16 15:35:14 +0200

    harpoon settings for mac

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,4 +8,8 @@ vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu)
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
+if vim.fn.has("mac") == 1 then
+    vim.keymap.set("n", "<C-'>", function() ui.nav_file(4) end)
+else
+    vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
+end
